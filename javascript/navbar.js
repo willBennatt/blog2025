eb264dfb54114adda696c7bf5b4a15d1c5de92fd
willBennatt <willbennatt@gmail.com>
2025-05-07 15:44:33 +0200

    Change color scheme to white with salmon highlights, add dark mode to blog page with section breaks, and fix toggle menu

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,10 +1,11 @@
 /* Responsive navbar hamburger menu for mobile screens */
 
-function toggleMenu() {
+let navbarButton = document.getElementById("navbar-hamburger-button");
+navbarButton.addEventListener("click", function () {
 	var navMenu = document.getElementById("navbar-hamburger");
 	if (navMenu.style.display === "block") {
 		navMenu.style.display = "none";
 	} else {
 		navMenu.style.display = "block";
 	}
-}
+});
